f8ffafa93914656ea4ab407b03463057d2a14e7e
David Day <dcday@themilwaukeeroad.com>
2019-04-06 11:45:51 -0500

    Add logging transfer script

---
+++ robotlogs.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Copying logs from roboRio"
+& scp rio:/home/lvuser/*.bag D:\logs *> $null
+Write-Host "Removing logs from roboRio"
+& ssh rio rm /home/lvuser/*.bag *> $null
+Write-Host "Creating html files"
+Get-ChildItem D:\logs\*.bag | ForEach-Object  { & D:\util\badlogvis.exe $_ }
+rm D:\logs\*.bag *> $nullgci
+& $(gci D:\logs\*.html | sort LastWriteTime | select -last 1)
