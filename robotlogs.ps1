2d094ee6a2fb09e56d524db2dc24a444a7a390cc
David Day <dcday@themilwaukeeroad.com>
2020-01-11 10:36:41 -0600

    Update robotlogs.ps1

--- robotlogs.ps1
+++ robotlogs.ps1
@@ -1,8 +1,9 @@
-﻿Write-Host "Copying logs from roboRio"
-& scp rio:/home/lvuser/*.bag C:\util\logs\ *> $null
+﻿$rio = 'rio'
+Write-Host "Copying logs from roboRio"
+& scp $rio`:/home/lvuser/*.bag d:\util\logs\
 Write-Host "Removing logs from roboRio"
-& ssh rio "rm /home/lvuser/*.bag" *> $null
+#& ssh $rio "rm /home/lvuser/*.bag" *> $null
 Write-Host "Creating html files"
-Get-ChildItem C:\util\logs\*.bag | ForEach-Object  { & C:\util\badlogvis.exe $_ }
-Remove-Item C:\util\logs\*.bag *> $null
-& $(Get-ChildItem C:\util\logs\*.html | Sort-Object LastWriteTime | Select-Object -last 1)
+Get-ChildItem d:\util\logs\*.bag | ForEach-Object { & d:\util\badlogvis.exe $_ }
+Remove-Item d:\util\logs\*.bag *> $null
+& $(Get-ChildItem d:\util\logs\*.html | Sort-Object LastWriteTime | Select-Object -last 1)
